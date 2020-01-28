d27bbe09a0a757af8a242ac152fe47f40d71fbc4
sergey <sergey@toposoft.lv>
2020-01-28 22:40:39 +0200

    add payments

--- microservice-aggregator/src/main/java/lv/toposoft/quarkus/microservices/client/PaymentTypeOneService.java
+++ microservice-aggregator/src/main/java/lv/toposoft/quarkus/microservices/client/PaymentTypeOneService.java
@@ -1,23 +1,4 @@
 package lv.toposoft.quarkus.microservices.client;
 
-import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-@Path("/api/payments")
-@Produces(MediaType.APPLICATION_JSON)
-@RegisterRestClient
-public interface PaymentTypeOneService {
-
-    @GET
-    Payment findAllPayments();
-
-    @GET
-    @Path("/{id}")
-    Payment findPaymentById(@PathParam("id") String id);
-
+public interface PaymentTypeOneService extends PaymentService {
 }
