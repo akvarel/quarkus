17d8d39e8987d7cbbd4bb0480eabe8267301ec53
sergey <sergey@toposoft.lv>
2020-01-29 23:27:47 +0200

    add payments

--- microservice-payment-type-two/src/main/java/lv/toposoft/quarkus/microservices/Payment.java
+++ microservice-payment-type-two/src/main/java/lv/toposoft/quarkus/microservices/Payment.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
+import java.time.Instant;
 
 /*
 Client should be able to create payment of one of 3 types - TYPE1, TYPE2, TYPE3. Fields 'amount' (positive decimal), 'currency' (EUR or USD), 'debtor_iban' and 'creditor_iban' (texts) are mandatory for all types.
@@ -33,6 +34,11 @@ public class Payment extends PanacheMongoEntity {
     public String creditor_bic;
     public String details;
 
+    public String status;
+    public Instant created;
+    public Instant canceled;
+    public BigDecimal cancellation_fee;
+
     @Override
     public String toString() {
         return "Payment{" +
@@ -43,6 +49,11 @@ public class Payment extends PanacheMongoEntity {
             ", creditor_iban='" + creditor_iban + '\'' +
             ", creditor_bic='" + creditor_bic + '\'' +
             ", details='" + details + '\'' +
+            ", status='" + status + '\'' +
+            ", created=" + created +
+            ", canceled=" + canceled +
+            ", cancellation_fee=" + cancellation_fee +
+            ", id=" + id +
             '}';
     }
 
